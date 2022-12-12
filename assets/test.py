b7395ac1ba2901b011873527695e1551dd26b567
ShushantSharma372 <91726756+ShushantSharma372@users.noreply.github.com>
2022-12-12 04:57:33 -0800

    Add files via upload

---
+++ assets/test.py
@@ -0,0 +1,14 @@
+import yfinance as yf
+
+msft = yf.Ticker("MSFT")
+
+# get stock info
+msft.info
+
+# get historical market data
+hist = msft.history(period="max")
+
+info = msft.info
+
+
+print(info)
